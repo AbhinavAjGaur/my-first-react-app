4426d763a4a73608fcbe4349172052ec06ce8cac
AbhinavAjGaur <abhinavajgaur@gmail.com>
2023-09-11 17:58:28 +0530

    chnage by pk

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React BY AJ
+          Learn React BY prateek
         </a>
       </header>
     </div>
